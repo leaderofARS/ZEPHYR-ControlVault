fbd36cede5eccc3b9abe9d236be1a14863bc6cc1
ARS <abhayrshanbhag@hotmail.com>
2025-12-09 14:01:32 +0530

    Add method signatures for AuthManager and AccessManager (no logic yet)

--- src/system/AuthManager.java
+++ src/system/AuthManager.java
@@ -1,8 +1,38 @@
 package system;
 
+import users.User;
+import exceptions.InvalidCredentialsException;
+import java.util.HashMap;
+
 public class AuthManager {
-    public boolean authenticate(String userId, String password) {
-        // Placeholder implementation
-        return false;
+
+    // User store
+    private HashMap<String, User> userMap;
+
+    public AuthManager() {
+        this.userMap = new HashMap<>();
+    }
+
+
+    // Loads all users from users.txt
+    public void loadUsersFromFile(String filePath) {
+        // TODO: implementation
+    }
+
+    // Validates login credentials & returns the User object
+    public User login(String username, String password) throws InvalidCredentialsException {
+        // TODO: implementation
+        return null;
+    }
+
+    // Creates and saves a new user
+    public void registerUser(User user) {
+        // TODO: implementation
+    }
+
+    // Helper to fetch a user by username
+    public User getUser(String username) {
+        // TODO: implementation
+        return null;
     }
 }
