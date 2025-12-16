a88ddeeaa6e50a9c8062d0bc471318580f603ced
ARS <abhayrshanbhag@hotmail.com>
2025-12-16 21:48:04 +0530

    Finalize Visitor badge lifecycle logic and override serialization

--- src/users/Visitor.java
+++ src/users/Visitor.java
@@ -1,40 +1,42 @@
 package users;
 
+import system.AccessZone;
+
 public class Visitor extends User {
 
-    private int badgeValidity; // remaining allowed accesses
+    private int badgeValidity;
 
     public Visitor(String userId, String username, String password, int badgeValidity) {
         super(userId, username, password, "VISITOR");
         this.badgeValidity = badgeValidity;
     }
 
-    // Getter & Setter
     public int getBadgeValidity() {
         return badgeValidity;
     }
 
-    public void setBadgeValidity(int badgeValidity) {
-        this.badgeValidity = badgeValidity;
-    }
-
-
     public void decrementBadgeValidity() {
-        if (badgeValidity > 0) {
-            badgeValidity--;
-        }
+        if (badgeValidity > 0) badgeValidity--;
     }
 
     public boolean isBadgeExpired() {
         return badgeValidity <= 0;
     }
 
-    // now production-ready)
+    @Override
+    public void accessArea(AccessZone zone) {
+        // No printing here
+    }
 
     @Override
-    public void accessArea(String zone) {
-        System.out.println("VISITOR " + username + " accessed: " + zone +
-                " | Remaining badge validity: " + badgeValidity);
+    public String serialize() {
+        return String.join(",",
+                userId,
+                username,
+                password,
+                role,
+                String.valueOf(badgeValidity)
+        );
     }
 
     @Override
